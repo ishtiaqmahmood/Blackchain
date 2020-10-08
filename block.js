10773bd45c18fb07cd38932a9b8016b05c4b6829
ishtiaqmahmood <ishtiaqmahmood22@gmail.com>
2020-10-08 19:15:08 +0600

    proof of work implementation done

--- block.js
+++ block.js
@@ -1,30 +1,56 @@
-const { GENESIS_DATA } = require('./config'); 
+const hexToBinary = require('hex-to-binary');
+const { GENESIS_DATA, MINE_RATE } = require('./config'); 
 const cryptoHash = require('./crypto-hash');
 
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
-   }
+    this.nonce = nonce;
+    this.difficulty = difficulty;
+  }
 
-   static genesis () {
-     return new this(GENESIS_DATA);
-   }
+  static genesis () {
+    return new this(GENESIS_DATA);
+  }
 
-   static mineBlock({ lastBlock, data }) {
-     const timestamp = Date.now();
-     const lastHash = lastBlock.hash;
+  static mineBlock({ lastBlock, data }) {
+    const lastHash = lastBlock.hash;
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
+    let nonce = 0;
 
-     return new this({
-       timestamp: Date.now(),
-       lastHash: lastBlock.hash,
-       data,
-       hash: cryptoHash(timestamp, lastHash, data)
-     });
-   }
+    do {
+      nonce++;
+      timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
+
+    return new this({
+      timestamp,
+      lastHash,
+      data,
+      difficulty,
+      nonce,
+      hash
+    });
+  }
+
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock;
+
+    if (difficulty < 1) return 1;
+
+    const difference = timestamp - originalBlock.timestamp;
+
+    if (difference > MINE_RATE) return difficulty - 1;
+
+    return difficulty + 1;
+  }
 }
 
 // const block1 = new Block({
